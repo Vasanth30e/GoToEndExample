d583da5abb715437b2cf639976c9b44c8c421dbe
unknown <vasanthgamer2000@gmail.com>
2023-07-11 12:22:07 +0530

    Example for goto end

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotoEndExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<10;i++)
+            {
+                if(i==6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value:{0}", i);
+            }
+        end: Console.WriteLine("The end");
+        Console.ReadKey();
+
+        }
+    }
+}
